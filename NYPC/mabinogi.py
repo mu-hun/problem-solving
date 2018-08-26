c8980cb2fb42ec3f8001f41113bdc20fdbca5fe2
Muhun Kim <iam@muhun.kim>
2018-08-26 23:03:31 +0900

    Update NYPC

--- NYPC/mabinogi.py
+++ NYPC/mabinogi.py
@@ -6,22 +6,17 @@
 # resources: 카드별 요구하는 자원의 배열
 
 def solve(N, resources):
-    resource = ['gold', 'mana', 'light', 'dark', 'nature']
-    sock_sung_list = [0, 0, 0, 0, 0]
-    sock_num = 0
+    sample_resource = ['gold', 'mana', 'light', 'dark', 'nature']
+    property_list = [0, 0, 0, 0, 0]
     if N > 12:
         return "invalid"
     for i in resources:
-        resource_location = resource.index(i)
-        sock_sung_list[resource_location] += 1
-    for sock_sung in sock_sung_list:
-        if not sock_sung == 0:
-            sock_num += 1
-    if sock_num < 4:
+        property_list[sample_resource.index(i)] += 1
+    if len(filter(lambda x: if x > 0)) < 4:
         return "valid"
     else:
         return "invalid"
-
+    
 N = int(input())                                 # stub
 resources = [input() for y in range(N)]             # stub
 
--- NYPC/postion.py
+++ NYPC/postion.py
@@ -7,14 +7,11 @@
 # H : 마실 포션의 회복량
 
 def solve(A, B, H):
-    sum_one = A + H
-    b_per_150 = int(B * 150 / 100)
+    sum_one, b_per_150 = A + H, int(B * 150 / 100)
     if sum_one < B:
         return sum_one
     elif sum_one < b_per_150:
-        b_out_value = sum_one - B
-        per_25 = b_out_value * 25 / 100
-        add_25_per = int(B + per_25)
+        add_25_per = int((sum_one - B) * 25 / 100) + B
         return add_25_per
     else:
         return b_per_150
