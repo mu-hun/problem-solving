238ab05b8495fb862a67ccadfdf293ae83699491
Muhun Kim <iam@muhun.kim>
2018-10-21 21:50:04 +0900

    Update acmicpc 10989

--- acmicpc.net/problem/10989.py
+++ acmicpc.net/problem/10989.py
@@ -1,6 +1,10 @@
-_list = []
-for i in range(int(input())):
-	_list.append(int(input()))
+import sys
+serises = [0 for __ in range(10001)]
 
-for i in sorted(_list):
-	print(i)
+for i in range(int(input())):
+	serises[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+	if not serises[i] is 0:
+		for j in range(serises[i]):
+			print(i)
