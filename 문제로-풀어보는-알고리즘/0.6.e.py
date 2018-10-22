1492c87af13361684e2bf9b0e5c33fdd85413c19
Muhun Kim <iam@muhun.kim>
2018-10-22 14:38:21 +0900

    Fix #2 0.6.e
    elemnets.pop() must be placed before return none

--- 문제로-풀어보는-알고리즘/0.6.e.py
+++ 문제로-풀어보는-알고리즘/0.6.e.py
@@ -1,32 +1,41 @@
 class Stack:
-    def __init__(self):
-        self.stack = []
-    
-    def push(self, val):
-        self.stack.append(val)
-    
-    def pop(self):
-        if self.is_empty():
-            return
-        self.stack.pop()
-    
-    def is_empty(self):
-        return len(self.stack) == 0
+	def __init__(self):
+		self.elements = []
+	 
+	def push(self, value):
+		self.elements.append(value)
+	
+	def pop(self):
+		if self.size() > 0:
+			return self.elements.pop()
+		else:
+			return None
+	
+	def size(self):
+		return len(self.elements)
 
 # Implement a queue with two stacks
 class Queue:
-    def __init__(self):
-        self.stackOne = Stack()
-        self.stackTwo = Stack()
-    
-    def enqueue(self, val):
-        self.stackOne.push(val)
-    
-    def dequeue(self):
-        if not self.stackTwo.size > 0:
-            self.move_context()
-        return self.stackTwo.pop()
-    
-    def move_context(self):
-        while self.stackOne.size > 0:
-            self.stackTwo.push(self.stackOne.pop())
+	def __init__(self):
+		self.inbox = Stack()
+		self.outbox = Stack()
+
+	def enqueue(self, value):
+		self.inbox.push(value)
+	
+	def dequeue(self):
+		if self.outbox.size() is 0:
+			while not self.inbox.size() is 0:
+				pop = self.inbox.pop()
+				self.outbox.push(pop)
+		return self.outbox.pop()
+
+if __name__ == '__main__':
+	q = Queue()
+	q.enqueue(1)
+	q.enqueue(2)
+	q.enqueue(3)
+	print(q.dequeue())
+	print(q.dequeue())
+	print(q.dequeue())
+
