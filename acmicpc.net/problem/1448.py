d8e658a2871242d5d9d928ece72bb3552cd150f5
Muhun Kim <iam@muhun.kim>
2019-07-16 19:11:42 +0900

    BOJ 1448

---
+++ acmicpc.net/problem/1448.py
@@ -0,0 +1,14 @@
+N = int(input())
+L = [int(input()) for __ in range(N)]
+A = -1
+
+L.sort()
+
+for i in range(N-1, 1, -1):
+    line_a = L[i]
+    line_b = L[i-1]
+    line_c = L[i-2]
+    if (line_a < line_b+line_c):
+        A = line_a+line_b+line_c
+        break
+print(A)
