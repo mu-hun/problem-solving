ba5b993d7fe3df07751e9037d2a797043479753b
Muhun Kim <iam@muhun.kim>
2019-07-16 19:12:44 +0900

    BOJ 10972
    이전, 다음 사전식 순열

---
+++ acmicpc.net/problem/10972.py
@@ -0,0 +1,26 @@
+def next_permutation(a):
+    i = len(a)-1
+    while i > 0 and a[i-1] >= a[i]:
+        i -= 1
+    if i <= 0:
+        return False
+    j = len(a)-1
+    while a[j] <= a[i-1]:
+        j -= 1
+
+    a[i-1],a[j] = a[j],a[i-1]
+
+    j = len(a)-1
+    while i < j:
+        a[i],a[j] = a[j],a[i]
+        i += 1
+        j -= 1
+
+    return True
+
+n = int(input())
+a = list(map(int,input().split()))
+if next_permutation(a):
+    print(' '.join(map(str,a)))
+else:
+    print(-1)
