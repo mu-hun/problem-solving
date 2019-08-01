09cf53506a1910a698c250ceea0bb5934308e58a
Muhun Kim <iam@muhun.kim>
2019-08-01 22:23:46 +0900

    BOJ 10828: use sys.stdin.readline

--- acmicpc.net/problem/10828.py
+++ acmicpc.net/problem/10828.py
@@ -1,36 +1,37 @@
 class Stack:
-	def __init__(self):
-		self.elements = []
-		self.key = {'pop':self.pop, 'size': self.size, 'empty': self.is_empty, 'top': self.top, 'push': self.push}
-	 
-	def push(self, value):
-		self.elements.append(value)
-	
-	def pop(self):
-		if self.is_empty():
-			return -1
-		return self.elements.pop()
-		
-	def size(self):
-		return len(self.elements)
+    def __init__(self):
+        self.elements = []
+        self.key = {'pop': self.pop, 'size': self.size,
+                    'empty': self.is_empty, 'top': self.top, 'push': self.push}
 
-	def is_empty(self):
-		if self.size() is 0:
-			return 1
-		else:
-			return 0
-	
-	def top(self):
-		if not self.is_empty():
-			return self.elements[-1]
-		else:
-			return -1
+    def push(self, value):
+        self.elements.append(value)
 
-s = Stack()
+    def pop(self):
+        if self.is_empty():
+            return -1
+        return self.elements.pop()
 
-for i in range(int(input())):
-	_in = input().split()
-	if _in[0] == 'push':
-		s.push(_in[1])
-	else:
-		print(s.key[_in[0]]())
+    def size(self):
+        return len(self.elements)
+
+    def is_empty(self):
+        if self.size() is 0:
+            return 1
+        return 0
+
+    def top(self):
+        if not self.is_empty():
+            return self.elements[-1]
+        return -1
+
+
+if __name__ == '__main__':
+    s = Stack()
+    from sys import stdin
+    for __ in range(int(stdin.readline().rstrip())):
+        _in = stdin.readline().rstrip().split()
+        if _in[0] == 'push':
+            s.push(int(_in[1]))
+            continue
+        print(s.key[_in[0]]())
