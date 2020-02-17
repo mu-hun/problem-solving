8c20f5b8b25f7f7c5b5339e7225b5e314f404ed7
Muhun Kim <iam@muhun.kim>
2020-02-17 13:42:31 +0900

    solved 42748

---
+++ programmers.com/learn/courses/30/lessons/42748.ts
@@ -0,0 +1,10 @@
+const sort = (a: number, b: number) => a - b
+
+export const solution = (
+  array: number[],
+  commands: [number, number, number][]
+) =>
+  commands.map(
+    command =>
+      array.slice(command[0] - 1, command[1]).sort(sort)[command[2] - 1]
+  )
