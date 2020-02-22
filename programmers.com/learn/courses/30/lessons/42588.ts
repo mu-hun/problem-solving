e14170939052fe52fc56084f2a491a2138ace650
Muhun Kim <iam@muhun.kim>
2020-02-22 20:55:06 +0900

    programmers(improve): 42588

--- programmers.com/learn/courses/30/lessons/42588.ts
+++ programmers.com/learn/courses/30/lessons/42588.ts
@@ -1,13 +1,8 @@
-export const solution = (heights: number[]) => {
-  let result: number[] = []
-  const hl = heights.length
-  for (let i = hl - 1; i >= 0; i--) {
-    for (let j = 1; j <= i; j++) {
-      if (heights[i] >= heights[i - j]) continue
-      result.push(i - j + 1)
-      break
+export const solution = (heights: number[]) =>
+  heights.map((v, i) => {
+    while (i >= 0) {
+      --i
+      if (v < heights[i]) return i + 1
     }
-    if (result.length !== hl - i) result.push(0)
-  }
-  return result.reverse()
-}
+    return 0
+  })
