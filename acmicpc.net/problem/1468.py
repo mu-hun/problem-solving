ffd0ef8b234d12547f4f13aa20950c46258a16f3
Muhun Kim <iam@muhun.kim>
2020-03-17 17:43:23 +0900

    BOJ(add): 1468
    Implement down-top solution

--- acmicpc.net/problem/1468.py
+++ acmicpc.net/problem/1468.py
@@ -21,11 +21,28 @@ def top(n: int, d: list):
     return d[n]
 
 
+def down(n: int):
+    d = [0]*(n+1)
+    d[1] = 0
+    for i in range(2, n+1):
+        d[i] = d[i-1] + 1
+        if i % 2 == 0 and d[i] > d[i//2] + 1:
+            d[i] = d[i//2] + 1
+        if i % 3 == 0 and d[i] > d[i//3] + 1:
+            d[i] = d[i//3] + 1
+    return d[n]
+
+
 def test_top():
     assert top(2, [None]*3) == 1
     assert top(10, [None]*11) == 3
 
 
+def test_down():
+    assert down(2) == 1
+    assert down(10) == 3
+
+
 if __name__ == "__main__":
     n = int(input())
-    print(top(n, [None]*n))
+    print(down(n))
