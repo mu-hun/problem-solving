1e36483fd63c787c9e036795bd4e5258f22bd44d
Muhun Kim <iam@muhun.kim>
2020-03-18 20:22:53 +0900

    BOJ: 9095
    Add DP implementation

--- acmicpc.net/problem/9095.py
+++ acmicpc.net/problem/9095.py
@@ -9,10 +9,29 @@ def go(sum: int, goal: int):
     return now
 
 
+def dp(n: int):
+    d = [0]*(n+1)
+    d[0] = 1
+    for i in range(1, n+1):
+        if i-1 >= 0:
+            d[i] += d[i-1]
+        if i-2 >= 0:
+            d[i] += d[i-2]
+        if i-3 >= 0:
+            d[i] += d[i-3]
+    return d[n]
+
+
 def test_go():
     assert go(0, 4) == 7
     assert go(0, 7) == 44
     assert go(0, 10) == 274
+
+
+def test_dp():
+    assert dp(4) == 7
+    assert dp(7) == 44
+    assert dp(10) == 274
 
 
 def main():
