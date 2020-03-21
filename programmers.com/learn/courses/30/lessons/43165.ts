7e6b0c14a6e41be0b879dc8ad4da8cfaa55e8a77
Muhun Kim <iam@muhun.kim>
2020-03-21 12:43:44 +0900

    programmers(improve): 43165
    Improve with Closure

--- programmers.com/learn/courses/30/lessons/43165.ts
+++ programmers.com/learn/courses/30/lessons/43165.ts
@@ -1,19 +1,13 @@
 export default function solution(numbers: number[], target: number) {
-  return go(0, 0, numbers, target)
-}
-
-const go = (
-  index: number,
-  cnt: number,
-  numbers: number[],
-  target: number
-): number => {
-  if (index == numbers.length) {
-    if (cnt == target) return 1
-    return 0
+  const go = (index: number, cnt: number): number => {
+    if (index == numbers.length) {
+      if (cnt == target) return 1
+      return 0
+    }
+    return (
+      go(index + 1, cnt + numbers[index]) + go(index + 1, cnt - numbers[index])
+    )
   }
-  return (
-    go(index + 1, cnt + numbers[index], numbers, target) +
-    go(index + 1, cnt - numbers[index], numbers, target)
-  )
+
+  return go(0, 0)
 }
