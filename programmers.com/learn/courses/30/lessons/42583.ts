68a3282cffc5a20f41d8b37a664306ebd2470a82
Muhun Kim <iam@muhun.kim>
2020-03-26 22:52:06 +0900

    programmers: 42583

--- programmers.com/learn/courses/30/lessons/42583.ts
+++ programmers.com/learn/courses/30/lessons/42583.ts
@@ -1,17 +1,26 @@
-const total = (bridgeCase: number[]) =>
-  bridgeCase.length === 0 || bridgeCase.reduce((a, b) => a + b)
-
 export default function solution(
   bridge_lenght: number,
   weight: number,
   truck_weights: number[]
 ) {
-  let timer = 0
-  let step = 0
-  let bridgeQueue: number[] = [],
-    weightQueue: number[] = []
+  let answer = 0
+  let total_truck_weight = 0
+  let bridge_queue: number[] = [],
+    weight_queue: number[] = []
 
-  do {} while (bridgeQueue.length > 0)
+  do {
+    for (const i in bridge_queue) bridge_queue[i]--
+    if (bridge_queue[0] === 0 && weight_queue) {
+      total_truck_weight -= weight_queue.shift()!
+      bridge_queue.shift()
+    }
 
-  return timer
+    if (total_truck_weight + truck_weights[0] <= weight) {
+      weight_queue.push(truck_weights[0])
+      bridge_queue.push(bridge_lenght)
+      total_truck_weight += truck_weights.shift()!
+    }
+    answer++
+  } while (bridge_queue.length > 0)
+  return answer
 }
