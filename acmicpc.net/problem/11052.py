6ec681522548f903a2a05cf0e04a76153b866420
Muhun Kim <iam@muhun.kim>
2020-04-16 21:04:47 +0900

    BOJ(Add test cases): 11052

--- acmicpc.net/problem/11052.py
+++ acmicpc.net/problem/11052.py
@@ -12,6 +12,9 @@ def test_buy_card():
     assert buy_card(4, [1, 5, 6, 7]) == 10
     assert buy_card(5, [10, 9, 8, 7, 6]) == 50
     assert buy_card(10, [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]) == 55
+    assert buy_card(10, [5, 10, 11, 12, 13, 30, 35, 40, 45, 47]) == 50
+    assert buy_card(4, [5, 2, 8, 10]) == 20
+    assert buy_card(4, [3, 5, 15, 16]) == 18
 
 
 if __name__ == "__main__":
