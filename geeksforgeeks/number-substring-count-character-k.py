2b1250c518c781826eaa7ba6d66f104f6a676940
Muhun Kim <iam@muhun.kim>
2020-05-08 12:17:26 +0900

    GeeksforGeeks: number substring count character
    Use set(list(string)) to remove duplicate item
    
    Move check() function to closure

--- geeksforgeeks/number-substring-count-character-k.py
+++ geeksforgeeks/number-substring-count-character-k.py
@@ -1,23 +1,25 @@
-def check(freq, k):
-    for char in freq:
-        if(char and char != k):
-            return False
-    return True
-
-
 def substringCount(s: str, k: int):
+    A = ord('a')
+
+    MAX_CHAR = max(map(lambda char: ord(char) - A, set(list(s)))) + 1
+
+    length = len(s)
     count = 0
 
-    MAX_CHAR = max(map(lambda x: ord(x) - ord('a'), [char for char in s])) + 1
+    def check(freq):
+        for count in freq:
+            if count > 0 and count != k:
+                return False
+        return True
 
-    for i in range(0, len(s)):
+    for i in range(length):
         freq = [0] * MAX_CHAR
-        for j in range(i, len(s)):
-            index = ord(s[j]) - ord('a')
+        for j in range(i, length):
+            index = ord(s[j]) - A
             freq[index] += 1
             if freq[index] > k:
                 break
-            if freq[index] == k and check(freq, k) == True:
+            if freq[index] == k and check(freq) == True:
                 count += 1
     return count
 
