edccc2b9e282ccc2001b7faed0ca293c50a13d6a
Muhun Kim <iam@muhun.kim>
2020-07-08 22:45:28 +0900

    BOJ: 14888 연산자 끼워넣기
    다시 풀어봤다. 개선해야 할 일: 이전에 쓰인 기저 생각해내기

--- acmicpc.net/problem/14888.py
+++ acmicpc.net/problem/14888.py
@@ -1,44 +1,45 @@
-def calc(a, index, cur, plus, minus, mul, div):
-    if index == len(a):
-        return (cur, cur)
-    res = []
-    if plus > 0:
-        res.append(calc(a, index+1, cur+a[index], plus-1, minus, mul, div))
-    if minus > 0:
-        res.append(calc(a, index+1, cur-a[index], plus, minus-1, mul, div))
-    if mul > 0:
-        res.append(calc(a, index+1, cur*a[index], plus, minus, mul-1, div))
-    if div > 0:
-        if cur >= 0:
-            res.append(
-                calc(a, index+1, cur//a[index], plus, minus, mul, div-1))
-        else:
-            res.append(
-                calc(a, index+1, -(-cur//a[index]), plus, minus, mul, div-1))
-    ans = (
-        max([t[0] for t in res]),
-        min([t[1] for t in res])
-    )
+from typing import List, Tuple
 
-    return ans
+
+def solution(A: List[int], operators: Tuple[int, int, int, int]):
+    results = []
+
+    def calc(A: List[int], index: int, current: int, plus: int, minus: int, mul: int, div: int):
+        if index == len(A):
+            results.append(current)
+            return
+        if plus > 0:
+            calc(A, index+1, current+A[index], plus-1, minus, mul, div)
+        if minus > 0:
+            calc(A, index+1, current-A[index], plus, minus-1, mul, div)
+        if mul > 0:
+            calc(A, index+1, current*A[index], plus, minus, mul-1, div)
+        if div > 0:
+            if current >= 0:
+                calc(A, index+1, current //
+                     A[index], plus, minus, mul, div-1)
+            else:
+                calc(A, index+1, -(-current //
+                                   A[index]), plus, minus, mul, div-1)
+    calc(A, 1, A[0], *operators)
+    return (max(results), min(results))
 
 
 def test_calc():
     one = [5, 6]
-    assert calc(one, 1, one[0], 0, 0, 1, 0) == (30, 30)
+    assert solution(one, (0, 0, 1, 0)) == (30, 30)
 
     two = [3, 4, 5]
-    assert calc(two, 1, two[0], 1, 0, 1, 0) == (35, 17)
+    assert solution(two, (1, 0, 1, 0)) == (35, 17)
 
     three = [1, 2, 3, 4, 5, 6]
-    assert calc(three, 1, three[0], 2, 1, 1, 1) == (54, -24)
+    assert solution(three, (2, 1, 1, 1)) == (54, -24)
 
 
 if __name__ == '__main__':
-    N = int(input())
+    input()
     A = list(map(int, input().split()))
-    plus, minus, mul, div = map(int, input().split())
 
-    ans = calc(A, 1, A[0], plus, minus, mul, div)
+    ans = solution(A, tuple(map(int, input().split())))
     print(ans[0])
     print(ans[1])
