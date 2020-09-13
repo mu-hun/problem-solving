f046d6457ba7c7fae524b706b9715d7c1e292215
Muhun Kim <iam@muhun.kim>
2020-09-13 17:43:28 +0900

    BOJ: 1107
    고장난 리모컨 버튼을 제외해서 원하는 TV 채널을 번호 입력과 채널 증가 +, 감소 -로 가장 적게 이동하는 수를 구하는 문제입니다.

--- acmicpc.net/problem/1107.py
+++ acmicpc.net/problem/1107.py
@@ -1,45 +1,41 @@
-C_MAX = 1000000
+from typing import Tuple
 
 
-def controller(goal: int, _broken: list):
-    broken = [False]*10
-    for index in _broken:
-        broken[index] = True
-
-    def possible(c: int):
-        if c == 0:
-            if broken[0]:
+def main(goal: int, brokens: Tuple[bool]):
+    def pressButton(subGoal: int):
+        if subGoal == 0:
+            if brokens[0]:
                 return 0
             return 1
-        length = 0
-        while c > 0:
-            if broken[c % 10]:
+        count = 0
+        while subGoal > 0:
+            if brokens[subGoal % 10]:
                 return 0
-            length += 1
-            c //= 10
-
-        return length
+            count += 1
+            subGoal //= 10
+        return count
 
     answer = abs(goal - 100)
-
-    for c in range(C_MAX+1):
-        length = possible(c)
-        if length == 0:
+    for subGoal in range(0, 1000001):
+        first_step = pressButton(subGoal)
+        if first_step == 0:
             continue
-        press = abs(c - goal)
-        if answer > length + press:
-            answer = length + press
+        second_step = abs(subGoal - goal)
+        if answer > first_step + second_step:
+            answer = first_step + second_step
     return answer
 
 
-def test_controller():
-    assert controller(5457, [6, 7, 8]) == 6
-    assert controller(100, [0, 1, 2, 3, 4]) == 0
-    assert controller(500000, [0, 2, 3, 4, 6, 7, 8, 9]) == 11117
+def test_main():
+    assert main(5457, (False, False, False, False,
+                       False, False, True, True, True, False)) == 6
 
 
 if __name__ == "__main__":
     goal = int(input())
-    _ = input()
-    brokens = map(int, input().split())
-    print(controller(goal, brokens))
+    count = int(input())
+    brokens = [False] * 10
+    if count > 0:
+        for index in map(int, input().split()):
+            brokens[index] = True
+    print(main(goal, tuple(brokens)))
